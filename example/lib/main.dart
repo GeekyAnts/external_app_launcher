6f84e48a1d061ea202cb5499e45491fb505f8efe
shubham <jaiswal.shubham84@gmail.com>
2020-07-07 11:05:07 +0530

    update package and resolve issues

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -16,6 +16,8 @@ class _MyAppState extends State<MyApp> {
     super.initState();
   }
 
+  Color containerColor = Colors.red;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -24,14 +26,31 @@ class _MyAppState extends State<MyApp> {
           title: const Text('Plugin example app'),
         ),
         body: Center(
-          child: RaisedButton(
-              onPressed: () async {
-                await LaunchVpn.openApp("com.example.corona_tracker", false);
-                // Enter thr package name of the App you want to open and for iOS add the URLscheme to the Info.plist file.
-                // The second arguments decide wether the app redirects PlayStore or AppStore.
-                // For testing purpose you can enter com.instagram.android
-              },
-              child: Center(child: Text("Open"))),
+          child: Container(
+            height: 50,
+            width: 150,
+            child: RaisedButton(
+                color: Colors.blue,
+                onPressed: () async {
+                  await LaunchVpn.openApp(
+                    androidPackageName: 'net.pulsesecure.pulsesecure',
+                    iosUrlScheme: 'pulsesecure://',
+                    appStoreLink:
+                        'itms-apps://itunes.apple.com/us/app/pulse-secure/id945832041',
+                    // openStore: false
+                  );
+                  // Enter thr package name of the App you want to open and for iOS add the URLscheme to the Info.plist file.
+                  // The second arguments decide wether the app redirects PlayStore or AppStore.
+                  // For testing purpose you can enter com.instagram.android
+                },
+                child: Container(
+                    child: Center(
+                  child: Text(
+                    "Open",
+                    textAlign: TextAlign.center,
+                  ),
+                ))),
+          ),
         ),
       ),
     );
