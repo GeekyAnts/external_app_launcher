50bf5a90dedd44392dbf6af52b4e7fa4aff8dd8d
Gayathri-GA <gayathrid@geekyants.com>
2023-04-06 17:12:22 +0530

    Completed adding native iOS code for passing and recieving data

--- ios/Classes/LaunchexternalappPlugin.m
+++ ios/Classes/LaunchexternalappPlugin.m
@@ -1,6 +1,7 @@
 #import "LaunchexternalappPlugin.h"
 
 @implementation LaunchexternalappPlugin
+
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
   FlutterMethodChannel* channel = [FlutterMethodChannel
       methodChannelWithName:@"launch_vpn"
@@ -14,39 +15,46 @@
 
   if ([@"getPlatformVersion" isEqualToString:call.method]) {
     result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
-  } else
-    if ([@"isAppInstalled" isEqualToString:call.method]) {
+  } else if ([@"isAppInstalled" isEqualToString:call.method]) {
+    
     if ([[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:call.arguments[@"package_name"]]]){
         result(@(YES));
     } else{
-        result(@(NO));}
-    
+        result(@(NO));
+    }
   } else if ([@"openApp" isEqualToString:call.method]) {
-     @try {
-        if ([[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:call.arguments[@"package_name"]]]) {
-           [[UIApplication sharedApplication] openURL:[NSURL URLWithString:call.arguments[@"package_name"]]];
-             result(@("app_opened"));
-        } else
-        {
-            NSLog(@"Is reaching here1");
-            if(![@"false" isEqualToString: call.arguments[@"open_store"]]) {
-                NSLog(@"Is reaching here2 %@", call.arguments[@"app_store_link"]);
+    @try {
+      if ([[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:call.arguments[@"package_name"]]]) {
+        // Create a dictionary with your data to be passed to the launched app
+        NSDictionary *dictionary = @{@"key": call.arguments[@"value"]};
+        // Convert the dictionary to NSData
+        NSData *data = [NSKeyedArchiver archivedDataWithRootObject:dictionary];
+        // Create an options dictionary with the data to be passed to the launched app
+        NSDictionary *options = @{UIApplicationLaunchOptionsURLKey: [NSURL URLWithString:call.arguments[@"package_name"]], UIApplicationLaunchOptionsAnnotationKey: data};
+        // Launch the app with the options dictionary
+        [[UIApplication sharedApplication] openURL:[NSURL URLWithString:call.arguments[@"package_name"]] options:options completionHandler:nil];
+        result(@("app_opened"));
+      } else {
 
-         [[UIApplication sharedApplication] openURL:[NSURL URLWithString:call.arguments[@"app_store_link"]]];
-            result(@("navigated_to_store"));
-            }
-            result(@("App not found in the device"));
+        NSLog(@"Is reaching here1");
+        if(![@"false" isEqualToString: call.arguments[@"open_store"]]) {
+          NSLog(@"Is reaching here2 %@", call.arguments[@"app_store_link"]);
+
+          [[UIApplication sharedApplication] openURL:[NSURL URLWithString:call.arguments[@"app_store_link"]]];
+          result(@("navigated_to_store"));
         }
-         }
+        result(@("App not found in the device"));
+      }
+    }
     @catch (NSException * e) {
-      NSLog(@"exception herre");
+      NSLog(@"exception here");
       result(e);
-    
+
     }
   } else {
     result(FlutterMethodNotImplemented);
   }
- 
+
 }
 
 @end
--- secondapp/ios/Runner/AppDelegate.swift
+++ secondapp/ios/Runner/AppDelegate.swift
@@ -3,11 +3,53 @@ import Flutter
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+    
+    let CHANNEL = "com.example.secondapp/platform"
+    
+    override func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        
+        let flutterViewController = window?.rootViewController as! FlutterViewController
+        
+        let channel = FlutterMethodChannel(name: CHANNEL, binaryMessenger: flutterViewController.binaryMessenger)
+        
+        channel.setMethodCallHandler { [weak self] (call: FlutterMethodCall, result: FlutterResult) -> Void in
+            
+            guard call.method == "getInitialIntent" else {
+                result(FlutterMethodNotImplemented)
+                return
+            }
+            
+            let data = self?.handleIntent(intent: launchOptions?[UIApplication.LaunchOptionsKey.userActivityDictionary] as? [AnyHashable : Any])
+            result(data)
+        }
+        
+        GeneratedPluginRegistrant.register(with: self)
+        
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
+    
+    override func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
+        
+        let data = handleIntent(intent: userActivity.userInfo)
+        
+        if let flutterViewController = window?.rootViewController as? FlutterViewController {
+            
+            let channel = FlutterMethodChannel(name: CHANNEL, binaryMessenger: flutterViewController.binaryMessenger)
+            
+            channel.invokeMethod("onNewIntent", arguments: data)
+        }
+        
+        return true
+    }
+    
+    private func handleIntent(intent: [AnyHashable : Any]?) -> String? {
+        if let data = intent?["data"] as? String {
+            print("Received data: \(data)")
+            return data
+        }
+        return nil
+    }
 }
