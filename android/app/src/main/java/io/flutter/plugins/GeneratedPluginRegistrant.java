91f3b44388a850ab8f061eb936cbcbca3df7bbcc
OmarQataberi <qataberio@gmail.com>
2025-02-26 17:40:28 +0300

    feat: implement LaunchExternalApp plugin for Android with app installation check and launch functionality

--- android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
+++ android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
@@ -1,25 +1,87 @@
-package io.flutter.plugins;
+package com.example.launchexternalapp;
 
-import io.flutter.plugin.common.PluginRegistry;
-import com.geekyants.launch_vpn.LaunchVpnPlugin;
+import androidx.annotation.NonNull;
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
+import io.flutter.plugin.common.MethodChannel.Result;
+import android.content.Context;
+import android.content.Intent;
+import android.text.TextUtils;
+import android.content.pm.PackageManager;
 
-/**
- * Generated file. Do not edit.
- */
-public final class GeneratedPluginRegistrant {
-  public static void registerWith(PluginRegistry registry) {
-    if (alreadyRegisteredWith(registry)) {
-      return;
-    }
-    LaunchVpnPlugin.registerWith(registry.registrarFor("com.geekyants.launch_vpn.LaunchVpnPlugin"));
+/** LaunchExternalAppPlugin */
+public class LaunchexternalappPlugin implements MethodCallHandler, FlutterPlugin {
+
+  private MethodChannel channel;
+  private Context context;
+
+  public LaunchexternalappPlugin() {
+    
   }
 
-  private static boolean alreadyRegisteredWith(PluginRegistry registry) {
-    final String key = GeneratedPluginRegistrant.class.getCanonicalName();
-    if (registry.hasPlugin(key)) {
-      return true;
+  @Override
+  public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {  
+    context = flutterPluginBinding.getApplicationContext();
+    channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "launch_vpn");
+    channel.setMethodCallHandler(this);
+  }
+
+  @Override
+  public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+    channel.setMethodCallHandler(null);
+  }
+
+  @Override
+  public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
+    if (call.method.equals("getPlatformVersion")) {
+      result.success("Android " + android.os.Build.VERSION.RELEASE);
+    } else if (call.method.equals("isAppInstalled")) {
+      if (!call.hasArgument("package_name") || TextUtils.isEmpty(call.argument("package_name").toString())) {
+        result.error("ERROR", "Empty or null package name", null);
+      } else {
+        String packageName = call.argument("package_name").toString();
+        result.success(isAppInstalled(packageName));
+      }
+    } else if (call.method.equals("openApp")) {
+
+      String packageName = call.argument("package_name");
+
+      result.success(openApp(packageName, call.argument("open_store").toString()));
+
+    } else {
+      result.notImplemented();
     }
-    registry.registrarFor(key);
-    return false;
+  }
+
+  private boolean isAppInstalled(String packageName) {
+    try {
+      context.getPackageManager().getPackageInfo(packageName, 0);
+      return true;
+    } catch (PackageManager.NameNotFoundException ignored) {
+      return false;
+    }
+  }
+
+  private String openApp(String packageName, String openStore) {
+    if (isAppInstalled(packageName)) {
+      Intent launchIntent = context.getPackageManager().getLaunchIntentForPackage(packageName);
+      if (launchIntent != null) {
+        // null pointer check in case package name was not found
+        launchIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(launchIntent);
+        return "app_opened";
+      }
+    } else {
+      if (!"false".equals(openStore)) {
+        Intent intent1 = new Intent(Intent.ACTION_VIEW);
+        intent1.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent1.setData(android.net.Uri.parse("https://play.google.com/store/apps/details?id=" + packageName));
+        context.startActivity(intent1);
+        return "navigated_to_store";
+      }
+    }
+    return "something went wrong";
   }
 }
